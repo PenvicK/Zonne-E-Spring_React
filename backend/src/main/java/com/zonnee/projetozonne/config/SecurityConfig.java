2c40fd857a1b42d58511534f9fad7187c8103cbf
[Vinicius Posser] <[viniciusposser37@gmail.com]>
2021-06-15 20:11:38 -0300

    add swagger2

--- backend/src/main/java/com/zonnee/projetozonne/config/SecurityConfig.java
+++ backend/src/main/java/com/zonnee/projetozonne/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -21,6 +22,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private Environment env;
+
+//    @Autowired
+//    private
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -41,4 +45,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
 }
